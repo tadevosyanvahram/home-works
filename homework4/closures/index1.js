d6162c5788b4a340d56567ce8f3d36e9240f28ae
Vahram Tadevosyan <tvahram313@gmail.com>
2023-09-09 16:16:31 +0400

    just commit

--- homework4/closures/index1.js
+++ homework4/closures/index1.js
@@ -1,12 +1,11 @@
 //Create a counter function using closure that increments a count every time it's called.
-let i = 0;
+
 function createCounter() {
+  let i = 0;
   return () => {
     return ++i;
   };
 }
 let counter = createCounter();
-counter();
-counter();
-counter();
+
 console.log(counter());
