db0f768c054c694e8bf2c58738e3971baa417ff7
Vahram Tadevosyan <tvahram313@gmail.com>
2023-09-09 15:31:09 +0400

    just commit

--- homework4/closures/index8.js
+++ homework4/closures/index8.js
@@ -1,14 +1,26 @@
 //Implement a currying function using closures.
 
-function curry(fn, ...args) {
-    if (args.length >= fn.length) {
+// function curry(fn, ...args) {
+//     if (args.length >= fn.length) {
+//       return fn(...args);
+//     } else {
+//       return function (...nextArgs) {
+//         return curry(fn, ...args, ...nextArgs);
+//       };
+//     }
+//   }
+
+function curry(fn) {
+  return function curred(...args) {
+    if (fn.length === args.length) {
       return fn(...args);
     } else {
-      return function (...nextArgs) {
-        return curry(fn, ...args, ...nextArgs);
-      };
+      return function (...newArgs) {
+        return curred(...args, ...newArgs);
+      }
     }
   }
+}
   
   function add(a, b, c) {
     return a + b + c;
